bd68ff1a98a435370496f2b4ba949ab0740c8e32
barvax <69000340+barvax@users.noreply.github.com>
2022-12-27 17:46:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const countryName = document.getElementById('country-name');
 const countryImg = document.getElementById('country-img');
 let falgsRoot = document.getElementById('flags-root')
  const select = document.getElementById('select');
-
+const mainBox = document.getElementById('main-box');
+const outerMainBox = document.getElementById('outer-main-box');
+const searchCountryBtn = document.getElementById('searchCountryBtn');
+const countryInput = document.getElementById('country-input');
  let country={}
  
  
@@ -13,7 +16,21 @@ function getAllCountriesLikeIWouldWriteIt() {
     response.json()
   );
 }
+function getByName(name) {
+  return fetch("https://restcountries.com/v2/name/"+name).then((response) =>
+    response.json()
+  );
+}
 
+searchCountryBtn.addEventListener('click',  ()=>{
+	
+	let name = countryInput.value;
+	 getByName(name).then((x)=>((x.map((item)=>(((item))))))).then((x) =>(spredAllFlags(getAllFlags(x))))
+	
+	
+	countryInput.value='';
+})
+ //
 
 getAllCountriesLikeIWouldWriteIt().then((actualCountries) => {
  
@@ -23,7 +40,8 @@ console.log(allCountries)
 }).then((actualCountries)=> {
 	
 	addNamesToSelectElement();
-	 spredAllFlags();
+	 spredAllFlags(getAllFlags(allCountries));
+ getCountriesByContinent('North America');
 })
 
 function getCountriesNames(){
@@ -43,59 +61,63 @@ function addNamesToSelectElement(){
 }
 
 
-function getAllFlags(){
-	let x =allCountries.map((item) => item.flags.png)
+function getAllFlags(arr){
+	let x =arr.map((item) => item.flags.png)
 	//console.log(x)
 	return x;
 }
-function spredAllFlags(){
+function spredAllFlags(arr){
 	let counter = 0;
-	let flags = getAllFlags();
-	console.log(flags)
-	falgsRoot.innerHTML = flags.map((flag)=>(` <img data-index=${counter++} class='flag-img' src="${flag}" ></img>`)).join("")
+	falgsRoot.innerHTML="";
+	//let flags = getAllFlags();
+	console.log(arr)
+	falgsRoot.innerHTML = arr.map((flag)=>(` <img data-index=${counter++} class='flag-img' src="${flag}" ></img>`)).join("")
 }
 
 select.onchange = (event) => {
      var inputText = event.target.value;
      console.log(inputText);
-	 countryName.innerHTML = inputText;
-	 findIndexByCountryName(inputText)
+	
+	 let country = (setCounteryObject(findIndexByCountryName(inputText)))
+	 console.log(country);
+	 BuildMainBoxData(country);
+	 
  }
  
  function findIndexByCountryName(countryName){
 	 for (let item in allCountries ){
 		 if(allCountries[item].name.common==countryName){
 			   let index =  item;
-  country = {
-	
-	 capital:allCountries[index].capital[0],
-	 car: allCountries[index].car.side,
-	 continents:allCountries[index].continents[0],
-	currenciesName:Object.values(allCountries[index].currencies)[0].name, 
-	currenciesSymbol:Object.values(allCountries[index].currencies)[0].symbol,//currencies:allCountries[index].currencies.XCD.name, //+ currencies.XCD.symbol,
-	 flags:allCountries[index].flags.png,
-	 languages:allCountries[index].languages.eng,
-	 population:allCountries[index].population,
-	 timezones:allCountries[index].timezones[0]
-	 
-	 
- }
- console.log(country);
+ setCounteryObject(index)
+
 			return item//the index in allCountries arr; 
 		 } 
 	 }
 //
  }
  
+  function getCountriesByContinent(continents){
+	  
+	 let x = allCountries.filter((item)=>(item.continents[0]==continents))
+	  console.log(x);
+  }
+ getCountriesByContinent('North America');
+ 
  falgsRoot.addEventListener('click',(e)=>{
-	   let index = e.target.closest("img[data-index]").dataset.index;
+	   let index = e.target.closest("img[data-index]").dataset.index;//צריך להעביר דגל ולא אינדקס ואז דרך הדגל לעשות חיפוש של אינדקס
+	 setCounteryObject(index);///צריך לסדר את זה למצוא קודם את האינדקס בתוך כל המערך ורק אז להעביר
+	 BuildMainBoxData(country);
+
+ })
+ 
+ function setCounteryObject(index){
 	     country = {
 	name: allCountries[index].name.common,
 	 capital:allCountries[index].capital[0],
 	 car: allCountries[index].car.side,
 	 continents:allCountries[index].continents[0],
 	currenciesName:Object.values(allCountries[index].currencies)[0].name, 
-	currenciesSymbol:Object.values(allCountries[index].currencies)[0].symbol,//currencies:allCountries[index].currencies.XCD.name, //+ currencies.XCD.symbol,
+	currenciesSymbol:Object.values(allCountries[index].currencies)[0].symbol,
 	 flags:allCountries[index].flags.png,
 	 languages:Object.values(allCountries[index].languages)[0],
 	 population:allCountries[index].population,
@@ -104,6 +126,35 @@ select.onchange = (event) => {
 	 
  }
  console.log(country)
+  return country;
+ }
+ 
+ mainBox.addEventListener('click',()=>{
+	 mainBox.style.visibility="hidden";
+	  outerMainBox.style.visibility="hidden";
  })
  
+ function BuildMainBoxData(country){
+	 mainBox.innerHTML=`
+	 <div class='box-inner-div'>
+	  <div>
+	 	 <h2> ${country.name}</h2>
+	  <h3>Capital: ${country.capital}</h3>
+	   <h3>Driving side: ${country.car}</h3>
+	    <h3>Continent: ${country.continents}</h3>
+		<h3>Currencie: ${country.currenciesName} ${country.currenciesSymbol}</h3>
+		   <h3>Languages: ${country.languages}</h3>
+		      <h3>Population: ${country.population}</h3>
+			     <h3>Timezones: ${country.timezones}</h3>
+	 </div>
+	  <div>
+	   <img class='box-img' src="${country.flags}"> </img>
+	 </div>
+	 </div>
+
+	 
+	 `;
+	  mainBox.style.visibility="visible";
+	 outerMainBox.style.visibility="visible";
+ }
  
